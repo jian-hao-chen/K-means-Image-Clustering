cb4f3a1282877c924cf0ae95808f00d4d1ee5cfa
jian-hao-chen <jhchen850910@icloud.com>
2018-03-29 20:53:00 +0800

    20180329 implemented initialize, shuffle dataset, mse.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
+#include <cmath>
 
 #define NUM_TOTAL_IMAGE 1048
+#define NUM_CLASS 6
 
 using namespace cimg_library;
 typedef CImg<unsigned char> Image;
@@ -39,7 +42,7 @@ int read_dataset(char *list, Image *dst, Feature *feature_vector)
         dst[idx].load(buffer);
         feature_vector[idx].name = buffer;
 
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < NUM_CLASS; i++)
         {
             // each feature_vector[idx].name have "./ObjectDataset/" which consists
             // of 15 charaters, so find the label at the 16th character.
@@ -80,9 +83,45 @@ int feature_extract(Image *src, Feature *dst, int num_image)
     return 0;
 }
 
-int k_means()
+int initialize(Feature *feature_arr, std::vector<Feature> *classes)
+{
+    // pick one feature of each class into vector as initial pattern.
+    int current_class = 0, idx = 0;
+    while (current_class < NUM_CLASS)
+    {
+        if (feature_arr[idx].label == current_class)
+        {
+            classes[current_class].push_back(feature_arr[idx]);
+            current_class++;
+        }
+        idx++;
+    }
+
+    return 0;
+}
+
+int k_means(Feature *feature_arr, std::vector<Feature> *classes)
 {
     return 0;
+}
+
+float mse(Feature x1, Feature x2)
+{
+    // loss function for k-means feature comparison
+    float result = 0;
+
+    for (int channel = 0; channel < 3; channel++)
+    {
+        result += std::pow((x1.mean[channel] - x2.mean[channel]), 2);
+        result += std::pow((x1.var[channel] - x2.var[channel]), 2);
+    }
+
+    for (int scale = 0; scale < 256; scale++)
+    {
+        result += std::pow((x1.histogram[scale] - x2.histogram[scale]), 2);
+    }
+
+    return std::sqrt(result);
 }
 
 int main()
@@ -90,7 +129,7 @@ int main()
     int ret;
     Image src[NUM_TOTAL_IMAGE];
     Feature feature_vector[NUM_TOTAL_IMAGE];
-    std::vector<Feature> classes[6];
+    std::vector<Feature> classes[NUM_CLASS];
     /* 
         There are 6 classes.
         0: airplane,
@@ -103,6 +142,10 @@ int main()
 
     ret = read_dataset("list.txt", src, feature_vector);
     ret = feature_extract(src, feature_vector, NUM_TOTAL_IMAGE);
+    ret = initialize(feature_vector, classes);
+
+    // shuffle the dataset to validation k-means algorithm.
+    std::random_shuffle(feature_vector, feature_vector + NUM_TOTAL_IMAGE);
 
     if (ret == 0)
         std::cout << "Done." << std::endl;
